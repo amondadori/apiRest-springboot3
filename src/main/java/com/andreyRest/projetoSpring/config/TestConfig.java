b2f3219026db23ffee56d5daf45dce48a759b943
amondadori <amondadori2@gmail.com>
2023-04-19 08:25:29 -0300

    Payment, one-to-one association

--- src/main/java/com/andreyRest/projetoSpring/config/TestConfig.java
+++ src/main/java/com/andreyRest/projetoSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.andreyRest.projetoSpring.entities.Category;
 import com.andreyRest.projetoSpring.entities.Order;
 import com.andreyRest.projetoSpring.entities.OrderItem;
+import com.andreyRest.projetoSpring.entities.Payment;
 import com.andreyRest.projetoSpring.entities.Product;
 import com.andreyRest.projetoSpring.entities.User;
 import com.andreyRest.projetoSpring.entities.enums.OrderStatus;
@@ -85,6 +86,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 		
 	}
 
